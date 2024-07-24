3a0cf664aed6aa9dcfefe63a4b1e6563703dbae1
bryan ruiz <andyruiz2468@gmail.com>
2024-07-24 15:49:04 -0600

    b

--- src/index.js
+++ src/index.js
@@ -5,32 +5,19 @@ const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 const apicache = require("apicache");
 const cache = apicache.middleware;
-// const v1router = require("./v1/routes/index.js");
 
 const v1workoutRouter = require("./v1/routes/workoutRoutes.js");
 const { swaggerDocs: V1swaggerDocs } = require("./v1/swagger.js");
 
 app.use(cors());
-
-// app.use("/api/v1",v1router);
-
-app.get('/', (req, res) => {
-    res.send("<h1>Hello world</h1>");
-})
-
-
-//middlware implemantation, every req pass for there, http req that are in json format, 
-//this middle transform ur json to an js obj and attched. 
 app.use(express.json());
-app.use(cache("2 minutes"));//be careful with this, sometimes doesnot work as it should, doesnt clear the cache sometimes??
+app.use(cache("2 minutes"));
+
 app.use("/api/v1/workouts", v1workoutRouter);
 
-
-
 app.listen(PORT, () => {
-
     console.log(`⚡ Server listening on port ${PORT}`);
     V1swaggerDocs(app, PORT);
 });
 
-export default app
+module.exports = app;
