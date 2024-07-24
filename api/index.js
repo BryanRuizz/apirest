df76711a30cfe34feb8ea399658dff7c2403a795
bryan ruiz <andyruiz2468@gmail.com>
2024-07-24 15:11:44 -0600

    vercel: test

--- api/index.js
+++ api/index.js
@@ -1,3 +1,25 @@
-import app from '../src/index';
+const express = require("express");
+const cors = require("cors");
+const apicache = require("apicache");
+const v1workoutRouter = require("../src/v1/routes/workoutRoutes.js");
+const { swaggerDocs: V1swaggerDocs } = require("../src/v1/swagger.js");
 
-export default app;
+const app = express();
+const cache = apicache.middleware;
+
+app.use(cors());
+app.use(express.json());
+app.use(cache("2 minutes"));
+
+app.use("/api/v1/workouts", v1workoutRouter);
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello world</h1>");
+});
+
+const PORT = process.env.PORT || 3000;
+V1swaggerDocs(app, PORT);
+
+module.exports = (req, res) => {
+    app(req, res);
+};
--- src/v1/swagger.js
+++ src/v1/swagger.js
@@ -1,6 +1,5 @@
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUI = require("swagger-ui-express");
-const { version } = require("uuid");
 
 // metadata info about our api
 const options = {
@@ -12,7 +11,7 @@ const options = {
         },
     },
     apis: ["src/v1/routes/workoutRoutes.js", "./src/database/Workout.js"],
-}
+};
 
 // Docs en json format
 const swaggerSpec = swaggerJSDoc(options);
@@ -22,11 +21,9 @@ const swaggerDocs = (app, port) => {
     app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
     app.get('/api/v1/docs.json', (req, res) => {
         res.setHeader('Content-Type', 'application/json');
-        res.send(swaggerSpec)
+        res.send(swaggerSpec);
     });
-    // console.log(`📓 Version 1 Docs are avaliable at http://localhost:${port}/api/v1/docs/`);
     console.log(`📓 Version 1 Docs are available at http://localhost:${port}/api/v1/docs/`);
 };
 
-
-module.exports = { swaggerDocs }
+module.exports = { swaggerDocs };
