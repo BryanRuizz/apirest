1699875e0ce86c989f9f5765e6702558ec50ecb3
bryan ruiz <andyruiz2468@gmail.com>
2024-07-24 15:37:08 -0600

    Update swagger.js

--- src/v1/swagger.js
+++ src/v1/swagger.js
@@ -1,8 +1,7 @@
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUI = require("swagger-ui-express");
-const { version } = require("uuid");
 
-// metadata info about our api
+// Metadata info about our API
 const options = {
     definition: {
         openapi: "3.0.0",
@@ -12,21 +11,19 @@ const options = {
         },
     },
     apis: ["src/v1/routes/workoutRoutes.js", "./src/database/Workout.js"],
-}
+};
 
-// Docs en json format
+// Docs en formato JSON
 const swaggerSpec = swaggerJSDoc(options);
 
-// function to setup our docs
+// Función para configurar nuestros docs
 const swaggerDocs = (app, port) => {
     app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
     app.get('/api/v1/docs.json', (req, res) => {
         res.setHeader('Content-Type', 'application/json');
-        res.send(swaggerSpec)
+        res.send(swaggerSpec);
     });
-    // console.log(`📓 Version 1 Docs are avaliable at http://localhost:${port}/api/v1/docs/`);
-    console.log(`📓 Version 1 Docs are available at http://localhost:${port}/api/v1/docs/`);
+    console.log(`📓 Version 1 Docs are available at http://localhost:${port}/api/v1/docs`);
 };
 
-
-module.exports = { swaggerDocs }
+module.exports = { swaggerDocs };
