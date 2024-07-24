aaf1b035176602c2ecc67ce32843e611375a4574
bryan ruiz <andyruiz2468@gmail.com>
2024-07-24 15:50:07 -0600

    c

--- src/v1/swagger.js
+++ src/v1/swagger.js
@@ -1,5 +1,6 @@
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUI = require("swagger-ui-express");
+const { version } = require("uuid");
 
 // metadata info about our api
 const options = {
@@ -10,8 +11,8 @@ const options = {
             version: "1.0.0"
         },
     },
-    apis: ["src/v1/routes/workoutRoutes.js", "src/database/Workout.js"],
-};
+    apis: ["src/v1/routes/workoutRoutes.js", "./src/database/Workout.js"],
+}
 
 // Docs en json format
 const swaggerSpec = swaggerJSDoc(options);
@@ -21,9 +22,11 @@ const swaggerDocs = (app, port) => {
     app.use('/api/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
     app.get('/api/v1/docs.json', (req, res) => {
         res.setHeader('Content-Type', 'application/json');
-        res.send(swaggerSpec);
+        res.send(swaggerSpec)
     });
+    // console.log(`📓 Version 1 Docs are avaliable at http://localhost:${port}/api/v1/docs/`);
     console.log(`📓 Version 1 Docs are available at http://localhost:${port}/api/v1/docs/`);
 };
 
-module.exports = { swaggerDocs };
+
+module.exports = { swaggerDocs }
