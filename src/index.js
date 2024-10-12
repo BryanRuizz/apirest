dec257e7cb04f78a42f17d6fe1575e6c752fa16b
bryan ruiz <bryanruiz2468@gmail.com>
2024-10-11 19:34:52 -0600

    Refactor: deleted apicache

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const cors = require("cors");
-const apicache = require("apicache");
-const cache = apicache.middleware;
+// const apicache = require("apicache");
+// const cache = apicache.middleware;
 
 const v1workoutRouter = require("./v1/routes/workoutRoutes.js");
 const { swaggerDocs: V1swaggerDocs } = require("./v1/swagger.js");
 
 app.use(cors());
 app.use(express.json());
-app.use(cache("2 minutes"));
+// app.use(cache("2 minutes"));
 
 app.get('/', (req, res) => {
     res.send("<h1>Hello world</h1>"); 
